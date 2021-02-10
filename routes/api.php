7d1c44e59db91ac92dba8d282a54667f6062eb40
Jacanroman <jacanroman@gmail.com>
2021-02-10 20:04:23 +0000

    adding the testing and the crud explanation

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
+Route::get('/testing-the-api',function(){
+    return ['message'=>'hello'];
+});
+*/
 
+
+/*posts
+CRUD IS BASICALLY
+1.get all (GET) /api/posts
+2.create a post (POST)  /api/posts
+3. get a single (GET)   /api/posts/{id}
+4. update a single (PUT) /api/posts/{id}
+5. delete (DELETE)      /api/posts/{id}
+*/
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
